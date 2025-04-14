c7c43d828e1b2f3b9ba80443f38cf8ef0633e8d9
Mr-Snows <Jayci.Nieves77@gmail.com>
2025-04-14 19:29:02 -0400

    calculator.py

--- calculator.py
+++ calculator.py
@@ -5,7 +5,36 @@ calculator.py
 One function per operation, in order.
 """
 # First example
+def square_root(a):
+    if a <  0:
+        raise ZeroDivisionError("Must be positive")
+    return math.sqrt(a)
+
+def hypotenuse(a, b):
+    return math.hypot(a, b)
+
 def add(a, b): 
-    pass
+    return a + b
+
+def subtract(a, b):
+    return a- b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Can't divide by zero")
+    return b / a
+
+def logarithm(a, b):
+    if a <= 0 or a == 1:
+        raise ValueError("Base can't be 1 and must be positive")
+    if b <= 0:
+        raise ValueError("Must be positive")
+    return math.log(b, a)
+
+def exponent(a, b):
+    return a ** b
 
 
