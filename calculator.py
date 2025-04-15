7f0b1360ae7f93390386ca81991cefff08f3daa9
jaycinieves <77039025+jaycinieves@users.noreply.github.com>
2025-04-14 23:55:45 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -2,45 +2,46 @@
 # Partner 1: Jayci Nieves
 # Partner 2: Jayci Nieves
 
-def square_root(a):
-    if a <  0:
-        raise ZeroDivisionError("Must be positive")
-    return math.sqrt(a)
+def calculator:
+    def square_root(a):
+        if a <  0:
+            raise ZeroDivisionError("Must be positive")
+        return math.sqrt(a)
 
-def hypotenuse(a, b):
-    return math.hypot(a, b)
+    def hypotenuse(a, b):
+        return math.hypot(a, b)
 
-def add(a, b): 
-    return a + b
+    def add(a, b): 
+        return a + b
 
-def subtract(a, b):
-    return a- b
+    def subtract(a, b):
+        return a- b
 
-def multiply(a, b):
-    return a * b
+    def multiply(a, b):
+        return a * b
 
-def divide(a, b):
-    if a == 0:
-        raise ZeroDivisionError("Can't divide by zero")
-    return b / a
+    def divide(a, b):
+        if a == 0:
+            raise ZeroDivisionError("Can't divide by zero")
+        return b / a
 
-def logarithm(a, b):
-    if a <= 0 or a == 1:
-        raise ValueError("Base can't be 1 and must be positive")
-    if b <= 0:
-        raise ValueError("Must be positive")
-    return math.log(b, a)
+    def logarithm(a, b):
+        if a <= 0 or a == 1:
+            raise ValueError("Base can't be 1 and must be positive")
+        if b <= 0:
+            raise ValueError("Must be positive")
+        return math.log(b, a)
 
-def exponent(a, b):
-    return a ** b
+    def exponent(a, b):
+        return a ** b
 
-def mul(a, b):
-    return a * b
+    def mul(a, b):
+        return a * b
 
-def div(a, b):
-    if a == 0:
-        raise ZeroDivisionError("Can't divide by zero")
-    return b / a
+    def div(a, b):
+        if a == 0:
+            raise ZeroDivisionError("Can't divide by zero")
+        return b / a
 
-def exp(a, b):
-    return a ** b
+    def exp(a, b):
+        return a ** b
