2340a69f3da1953e6b7fa9eabb07cae6a76ba4ad
jaycinieves <77039025+jaycinieves@users.noreply.github.com>
2025-04-14 23:23:23 -0400

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -8,42 +8,51 @@ from calculator import *
 class TestCalculator(unittest.TestCase):
 
 def test_add(self):
-    self.assertEqual(add(10, 5), 15)
-    self.assertEqual(add(-1, -4), -5)
+    self.assertEqual(calculator.add(10, 5), 15)
+    self.assertEqual(calculator.add(-10, -5), -15)
+    self.assertEqual(calculator.add(-5, 5), 0)
+    self.assertEqual(calculator.add(2.5, 2.5), 5)
 
 def test_subtract(self):
-    self.assertEqual(subtract(7, 2), 5)
-    self.assertEqual(subtract(2, 7), -5)
-
+    self.assertEqual(calculator.subtract(10, 5), 5)
+    self.assertEqual(calculator.subtract(-10, -5), 5)
+    self.assertEqual(calculator.subtract(-5, 5), -10)
+    self.assertEqual(calculator.subtract(2.5, 2.5), 0)
+    
 def test_divide_by_zero(self):
-    with self.assertRaises(ZeroDivisionError):
-        divide(0, 5)
-
+    self.assertRaises(ValueError, calculator.divide, 0, 10)
+    
 def test_logarithm(self):
-    self.assertEqual(logarithm(2, 8), 3)
+     self.assertEqual(calculator.logarithm(2, 8)
 
 def test_log_invalid_base(self):
-    self.assertFalse(logarithm(-2, 5) is None)
-    self.assertFalse(logarithm(1, 5) is None)
+    self.assertRaises(ValueError, calculator.logarithm, -1, 1)
+    self.assertRaises(ValueError, calculator.logarithm,  0, 1)
+    self.assertRaises(ValueError, calculator.logarithm, 1, 1)
 
 def test_multiply(self):
-    self.assertEqual(multiply(1, 5), 5)
-    self.assertEqual(multiply(-1, 5), -5)
+    self.assertEqual(calculator.multiply(1, 5), 5)
+    self.assertEqual(calculator.multiply(0, 5), 0)
+    self.assertEqual(calculator.multiply(-1, 5), -5)
+    self.assertEqual(calculator.multiply(10, 2), 20)
 
 def test_divide(self):
-    self.assertEqual(divide(1, 5), 5)
-    self.assertEqual(divide(-5, 15), -3)
+    self.assertEqual(calculator.divide(1, 5), 5)
+    self.assertEqual(calculator.divide(0, 5), 0)
+    self.assertEqual(calculator.divide(2, 10), 5)
+    self.assertEqual(calculator.divide(-1, 5), -5)
 
 def test_log_invalid_argument(self):
-    with self.assertRaises(ZeroDivisionError):
-        logarithm(2,-5)
+    self.assertRaises(ValueError, calculator.logarithm, 2, 0)
+    self.assertRaises(ValueError, calculator.logarithm, 2, -1)
 
 def test_hypotenuse(self):
-    self.assertEqual(hypotenuse(3, 4), 5)
+    self.assertEqual(calculator.hypotenuse(3, 4), 5)
+    self.assertEqual(calculator.hypotenuse(-5), 5)
+
 
 def test_sqrt(self):
-    self.assertEqual(square_root(4), 2)
-   self.assertFalse(square_root(-4) is None)
+    self.assertEqual(calculator.square_root(4), 2)
 
 # Do not touch this
 if __name__ == "__main__":
